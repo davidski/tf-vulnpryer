707234e2b6a447c9b573927d8eff783f944bebf8
David F. Severski <davidski@deadheaven.com>
2019-03-23 14:20:31 -0700

    Add AWS provider version pin.

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
+  version = "~> 1.54"
 
   assume_role {
     role_arn = "arn:aws:iam::754135023419:role/administrator-service"
